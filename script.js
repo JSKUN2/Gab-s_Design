e1ca1dcd951939c5546f2ec86f84510c988e2549
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2023-02-22 18:17:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function home(){
+    document.getElementById('screen').src = 'home.html';
+}
+function product(){
+    document.getElementById('screen').src = 'product.html';
+}
+function about(){
+    document.getElementById('screen').src = 'about.html';
+}
