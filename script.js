9b8af01855a529f3d38f635286d84ce8c3cbe494
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2023-02-25 22:50:58 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function home(){
-    document.getElementById('screen').src = 'home.html';
-}
-function product(){
-    document.getElementById('screen').src = 'product.html';
-}
-function about(){
-    document.getElementById('screen').src = 'about.html';
-}
